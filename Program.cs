e00667fcf9fff0d47a5ec2c2ac99b7e9bdff3b99
sunisdream <61583542+sunisdream@users.noreply.github.com>
2020-06-05 17:18:05 +0300

    Added a project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab04___1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Student student = new Student();
+
+			Console.WriteLine("Enter student's name:");
+			student.SetName(Console.ReadLine());
+
+			Console.WriteLine("Enter student's surname:");
+			student.SetLastName(Console.ReadLine());
+
+			Console.WriteLine("Enter student's group name:");
+			student.SetGroup(Console.ReadLine());
+
+			Console.WriteLine("Enter student's year:");
+			student.SetYear(Convert.ToInt32(Console.ReadLine()));
+
+			Console.WriteLine("Enter student's address:");
+			student.SetAddress(Console.ReadLine());
+
+			Console.WriteLine("Enter student's passport id:");
+			student.SetPassport(Convert.ToInt32(Console.ReadLine()));
+
+			Console.WriteLine("Enter student's age:");
+			student.SetAge(Convert.ToInt32(Console.ReadLine()));
+
+			Console.WriteLine("Enter student's phone number:");
+			student.SetTelephone(Convert.ToInt32(Console.ReadLine()));
+
+			Console.WriteLine("Enter student's rating:");
+			student.SetRating(Convert.ToInt32(Console.ReadLine()));
+
+			student.StudentRating(student.GetRating());
+		}
+	}
+}
---
+++ Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04___1
+{
+	class Student
+	{
+		private string name;
+		private string lastName;
+		private string group;
+		private int year;
+		private string address;
+		private int passport;
+		private int age;
+		private int telephone;
+		private int rating;
+
+		public Student() { }
+
+		public string GetName() { return name; }
+		public string GetLastName() { return lastName; }
+		public string GetGroup() { return group; }
+		public int GetYear() { return year; }
+		public string GetAddress() { return address; }
+		public int GetPassport() { return passport; }
+		public int GetAge() { return age; }
+		public int GetTelephone() { return telephone; }
+		public int GetRating() { return rating; }
+		public void SetName(string name) { this.name = name; }
+		public void SetLastName(string surname) { lastName = surname; }
+		public void SetGroup(string group) { this.group = group; }
+		public void SetYear(int year) { this.year = year; }
+		public void SetAddress(string address) { this.address = address; }
+		public void SetPassport(int passportId) { passport = passportId; }
+		public void SetAge(int age) { this.age = age; }
+		public void SetTelephone(int phoneNumber) { telephone = phoneNumber; }
+		public void SetRating(int rating) { this.rating = rating; }
+
+		public void StudentRating(int R)
+		{
+			if (rating >= 90)
+			{
+				Console.WriteLine("Вітаємо відмінника");
+			}
+			else if (rating >= 75)
+			{
+				Console.WriteLine("Можна вчитися краще");
+			}
+			else
+			{
+				Console.WriteLine("Варто більше уваги приділяти навчанню");
+			}
+		}
+	}
+}
